16610092dad93bf7443fc535a769dbd934fd0bc4
Tin Tran <thuutin@gmail.com>
2016-07-07 16:59:28 +0700

    Get topstories from hackernews API

--- app/src/main/java/me/tintran/hackernews/data/HackerNewsApi.java
+++ app/src/main/java/me/tintran/hackernews/data/HackerNewsApi.java
@@ -2,15 +2,14 @@ package me.tintran.hackernews.data;
 
 import java.util.List;
 import retrofit2.Call;
+import retrofit2.http.GET;
 
 /**
  * Created by tin on 7/6/16.
  */
 
 public interface HackerNewsApi {
-  Call<List<TopStories>> getTopStories();
+  @GET(value = "topstories.json")
+  Call<int[]> getTopStories();
 
-  class TopStories {
-    int id;
-  }
 }
--- app/src/prod/java/me/tintran/hackernews/StoriesRepository.java
+++ app/src/prod/java/me/tintran/hackernews/StoriesRepository.java
@@ -3,16 +3,25 @@ package me.tintran.hackernews;
 import android.os.Bundle;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
+import android.util.Log;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import me.tintran.hackernews.data.HackerNewsApi;
 import me.tintran.hackernews.data.Item;
 import me.tintran.hackernews.data.TopStoriesUseCase;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * Created by tin on 7/6/16.
  */
 
 public class StoriesRepository implements
-    TopStoriesUseCase, LoaderManager.LoaderCallbacks<List<Item>> {
+    TopStoriesUseCase {
 
   private LoaderManager loaderManager;
   private Callback callback;
@@ -23,19 +32,25 @@ public class StoriesRepository implements
 
   @Override public void getTopStories(final Callback callback) {
     this.callback = callback;
-    loaderManager.initLoader(0, null, this);
+    Retrofit retrofit = new Retrofit.Builder()
+        .baseUrl("https://hacker-news.firebaseio.com/v0/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build();
+    HackerNewsApi hackerNewsApi = retrofit.create(HackerNewsApi.class);
+    Call<int[]> topStories = hackerNewsApi.getTopStories();
+    topStories.enqueue(new retrofit2.Callback<int[]>() {
+      @Override public void onResponse(Call<int[]> call, Response<int[]> response) {
+        List<Item> items = new ArrayList<>(response.body().length);
+        for (int i = 0; i < response.body().length; i++) {
+          items.add(new Item(String.valueOf(response.body()[i]), "Subtitle " + i));
+        }
+        callback.onComplete(items);
+      }
+
+      @Override public void onFailure(Call<int[]> call, Throwable t) {
+        Log.d("StoriesRepository" , "Failure " + t.getMessage());
+      }
+    });
   }
 
-  @Override public Loader<List<Item>> onCreateLoader(int id, Bundle args) {
-    return null;
-  }
-
-  @Override public void onLoadFinished(Loader<List<Item>> loader, List<Item> data) {
-    callback.onComplete(data);
-    loaderManager.destroyLoader(0);
-  }
-
-  @Override public void onLoaderReset(Loader<List<Item>> loader) {
-
-  }
 }
